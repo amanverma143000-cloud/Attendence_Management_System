d9440685e16cca1b92b9c2518c1a27763bdf37b0
Aman Verma <amanverma143000@gmail.com>
2026-06-11 22:49:56 +0530

    Updated frontend and backend

--- client/src/app/apiSlice.js
+++ client/src/app/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: '/api',
+    baseUrl: 'https://attendence-management-system-abyo.onrender.com/api',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) headers.set('authorization', `Bearer ${token}`);
